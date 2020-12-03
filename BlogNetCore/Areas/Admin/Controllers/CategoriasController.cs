800859b4d5cbfb06e3275d123549e1d6548eb9c3
JimcostDev <rjak727@gmail.com>
2020-12-03 15:24:42 -0500

    Crear controlador de categorias - agregar metodo GetAll

---
+++ BlogNetCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,78 @@
+﻿using BlogNetCore.AccesoDatos.Data.Repository;
+using BlogNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo)
+        {
+            //con esto puedo acceder a todas las entedidades
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /*********************************** CREAR CATEGORIA ********************************************/
+        //mostar formulario para crear una nueva categoria, por ello es de tipo HttpGet
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Categoria categoria)
+        {
+            //validar el modelo - validar que los campos obligatorios se envien (DataAnnontations)
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Add(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+        /*********************************** EDITAR CATEGORIA ********************************************/
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Categoria categoria = new Categoria();
+            categoria = _contenedorTrabajo.Categoria.Get(id);//buscar categoria por su id
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categoria categoria)
+        {
+            //validar el modelo - validar que los campos obligatorios se envien (DataAnnontations)
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Update(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+        #region  LLAMADAS A LA API
+        //OBTENER TODOS LOS DATOS DE LA ENTIDAD
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+        #endregion
+
+    }
+}
