a97a8bb5d3e7b25a1d18ee34671cf2a63986686f
JimcostDev <rjak727@gmail.com>
2020-12-10 14:16:50 -0500

    correcion controlador articulos

--- BlogNetCore/Areas/Admin/Controllers/ArticulosController.cs
+++ BlogNetCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-﻿using BlogNetCore.AccesoDatos.Data.Repository;
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BlogNetCore.AccesoDatos.Data.Repository;
->>>>>>> 1326d8e62ff5a1da37f3a8bd91b58e84aa4cd109
 using BlogNetCore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace BlogNetCore.Areas.Admin.Controllers
 {
-<<<<<<< HEAD
+
     [Area("Admin")]
-=======
->>>>>>> 1326d8e62ff5a1da37f3a8bd91b58e84aa4cd109
+
+
     public class ArticulosController : Controller
     {
         private readonly IContenedorTrabajo _contenedorTrabajo;
@@ -33,38 +26,18 @@ namespace BlogNetCore.Areas.Admin.Controllers
             {
                 Articulo = new Models.Articulo(),
                 ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
-<<<<<<< HEAD
+
             };
             return View(articuloViewModel);
-=======
-        };
-        return View(articuloViewModel);
-            
->>>>>>> 1326d8e62ff5a1da37f3a8bd91b58e84aa4cd109
+
         }
-        #region  LLAMADAS A LA API
+        #region LLAMADAS A LA API
         //OBTENER TODOS LOS DATOS DE LA ENTIDAD
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties: "Categoria") });
-        }
-
-        //[HttpDelete]
-        //public IActionResult Delete(int id)
-        //{
-
-        //    var objFromDb = _contenedorTrabajo.Categoria.Get(id);//buscar categoria por su id
-        //    if (objFromDb == null)
-        //    {
-        //        return Json(new { success = false, message = "Error al borrar categoría" });
-        //    }
-        //    _contenedorTrabajo.Categoria.Remove(objFromDb);
-        //    _contenedorTrabajo.Save();
-        //    return Json(new { success = true, message = "Se elimino categoría correctamente." });
-
-        //    
-        //}
+        } 
         #endregion
     }
 }
