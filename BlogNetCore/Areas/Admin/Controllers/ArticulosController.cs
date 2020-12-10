038b7667fd9b34278c217f84f7bffa947c186cb6
JimcostDev <rjak727@gmail.com>
2020-12-10 13:07:07 -0500

    Crear controlador Articulo, ViewModel y vista Index

---
+++ BlogNetCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogNetCore.AccesoDatos.Data.Repository;
+using BlogNetCore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogNetCore.Areas.Admin.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            //con esto puedo acceder a todas las entedidades
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //instaciamos el viewmodel para poder obtener la informacion de categoria
+            ArticuloViewModel articuloViewModel = new ArticuloViewModel()
+            {
+                Articulo = new Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+        };
+        return View(articuloViewModel);
+            
+        }
+        #region  LLAMADAS A LA API
+        //OBTENER TODOS LOS DATOS DE LA ENTIDAD
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties: "Categoria") });
+        }
+
+        //[HttpDelete]
+        //public IActionResult Delete(int id)
+        //{
+
+        //    var objFromDb = _contenedorTrabajo.Categoria.Get(id);//buscar categoria por su id
+        //    if (objFromDb == null)
+        //    {
+        //        return Json(new { success = false, message = "Error al borrar categoría" });
+        //    }
+        //    _contenedorTrabajo.Categoria.Remove(objFromDb);
+        //    _contenedorTrabajo.Save();
+        //    return Json(new { success = true, message = "Se elimino categoría correctamente." });
+
+        //    
+        //}
+        #endregion
+    }
+}
---
+++ BlogNetCore/wwwroot/js/articulo.js
@@ -0,0 +1,71 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    cargarDatatable();
+});
+
+
+function cargarDatatable() {
+    dataTable = $("#tblArticulos").DataTable({
+        //CUERPO DEL DATATABLE
+        "ajax": {
+            "url": "/admin/articulos/GetAll",//RUTA DE DONDE OBTIENE LOS DATOS
+            "type": "GET",//COMO VAMOS A CONSULTAR DATOS ES DE TIPO GET
+            "datatype": "json"
+        },
+        "columns": [
+            //CAMPOS DE LA ENTIDAD
+            { "data": "id", "width": "5%" },
+            { "data": "nombre", "width": "25%" },
+            { "data": "categoria.nombre", "width": "25%" },
+            { "data": "fechaCreacion", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    //CONCATENAR DE UNA FORMA MAS SIMPLE - template literals `COMILLAS INVERTIDAS` 
+                    return `<div class="text-center">
+                            <a href='/Admin/Articulos/Edit/${data}' class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                            <i class='fas fa-edit'></i> Editar
+                            </a>
+                            &nbsp;
+                            <a onclick=Delete("/Admin/Articulos/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                            <i class='fas fa-trash-alt'></i> Borrar
+                            </a>
+                            `;
+                }, "width": "25%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No hay registros"
+        },
+        "width": "100%"
+    });
+}
+
+//UTILIZANDO SWEETALERT
+function Delete(url) {
+    swal({
+        title: "Esta seguro de borrar?",
+        text: "Este contenido no se puede recuperar!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#FF1D10",
+        confirmButtonText: "Si, borrar!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {//VALIDAMOS QUE SI SE BORRO
+                if (data.success) {
+                    toastr.success(data.message);//UTILIZANDO TOASTR
+                    dataTable.ajax.reload();//RECARGAR
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}
