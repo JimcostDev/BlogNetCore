4f40e88745206134e95cf1f56fd0033dd0ec5ad7
JimcostDev <rjak727@gmail.com>
2020-12-30 13:19:23 -0500

    Crear controlador para slider y vista principal - index

---
+++ BlogNetCore/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogNetCore.AccesoDatos.Data.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogNetCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SlidersController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public SlidersController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnvironment)
+        {
+            //con esto puedo acceder a todas las entedidades
+            _contenedorTrabajo = contenedorTrabajo;
+            //para poder subir las imagenes
+            _hostingEnvironment = hostingEnvironment;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region LLAMADAS A LA API
+        //OBTENER TODOS LOS DATOS DE LA ENTIDAD
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Slider.GetAll() });
+        }
+        #endregion
+    }
+}
