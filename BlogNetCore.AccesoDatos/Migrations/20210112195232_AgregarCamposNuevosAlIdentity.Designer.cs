c12ab49de8afbdc669e6f6cb9243b5420f17e605
JimcostDev <rjak727@gmail.com>
2021-01-12 14:54:39 -0500

    Agregar nuevos campos al Identity

--- BlogNetCore.AccesoDatos/Migrations/20210112195232_AgregarCamposNuevosAlIdentity.Designer.cs
+++ BlogNetCore.AccesoDatos/Migrations/20210112195232_AgregarCamposNuevosAlIdentity.Designer.cs
@@ -4,14 +4,16 @@ using BlogNetCore.AccesoDatos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogNetCore.AccesoDatos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210112195232_AgregarCamposNuevosAlIdentity")]
+    partial class AgregarCamposNuevosAlIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BlogNetCore.Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogNetCore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "La ciudad es requerida")]
+        public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El país es requerido")]
+        public string Pais { get; set; }
+    }
+}
