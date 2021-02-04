639f02c24520ae244489080a932e4b0ffb7cfa23
JimcostDev <rjak727@gmail.com>
2021-02-04 11:51:33 -0500

    Implementar metodo de clase InicializadoraDB

--- BlogNetCore.AccesoDatos/Data/Inicializador/InicializadorDB.cs
+++ BlogNetCore.AccesoDatos/Data/Inicializador/InicializadorDB.cs
@@ -1,10 +1,59 @@
-﻿using System;
+﻿using BlogNetCore.Models;
+using BlogNetCore.Utilidades;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlogNetCore.AccesoDatos.Data.Inicializador
 {
-    public class InicializadorDB
+    public class InicializadorDB : IInicializadorDB
     {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public InicializadorDB(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public void Inicializar()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            if (_db.Roles.Any(ro => ro.Name == CNT.Admin))
+                return;
+
+            _roleManager.CreateAsync(new IdentityRole(CNT.Admin)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(CNT.Usuario)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "admin@jimcostdev.com",
+                Email = "admin@jimcostdev.com",
+                EmailConfirmed = true,
+                Nombre = "JimcostDev",
+
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            ApplicationUser usuario = _db.ApplicationUser
+                .Where(us => us.Email == "admin@jimcostdev.com")
+                .FirstOrDefault();
+            _userManager.AddToRoleAsync(usuario, CNT.Admin).GetAwaiter().GetResult();
+
+        }
     }
 }
